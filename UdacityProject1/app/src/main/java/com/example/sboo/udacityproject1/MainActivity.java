46364533c84597e3588f8be769153d3d5c952b02
sboo <sboo@ebaykorea.com>
2016-08-02 16:57:12 +0900

    Add button and variable

--- UdacityProject1/app/src/main/java/com/example/sboo/udacityproject1/MainActivity.java
+++ UdacityProject1/app/src/main/java/com/example/sboo/udacityproject1/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.sboo.udacityproject1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View v){
+        int numberOfcoffees = 2;
+        display(numberOfcoffees);
+        displayPrice(numberOfcoffees * 5);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
